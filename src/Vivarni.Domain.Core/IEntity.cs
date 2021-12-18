549b06f2cb5080f882a4d46f8aefb8ccb3193d4a
Michaël Vittorelli <vittorelli@gmail.com>
2021-12-18 22:35:06 +0100

    Update NuGet package metadata

--- src/Vivarni.Domain.Core/IEntity.cs
+++ src/Vivarni.Domain.Core/IEntity.cs
@@ -1,12 +1,12 @@
 ﻿namespace Vivarni.Domain.Core
 {
     /// <summary>
-    /// An entity which is mapped to the databasse via Entity Framework.
+    /// An entity which is mapped to the database.
     /// </summary>
     public interface IEntity { }
 
     /// <summary>
-    /// In support for database tables which don't use bigint as primary key data type. Direct usage
+    /// In support for database tables that don't use bigint as primary key data type. Direct usage
     /// of this interface should be avoided; <see cref="BaseEntity"/> should be favored in stead.
     /// We allow different primary key data types untill we have a more stable ground to refactor
     /// even further.
