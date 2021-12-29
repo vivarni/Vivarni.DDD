d31c7396200fc71326b31a9de7064b3a4f5714cb
Michaël Vittorelli <vittorelli@gmail.com>
2021-12-29 09:01:59 +0100

    IEntityWithDomainEvents: Split interface in order to facilitate event dispatching with EF

--- src/Vivarni.Domain.Core/IEntityWithDomainEvents.cs
+++ src/Vivarni.Domain.Core/IEntityWithDomainEvents.cs
@@ -1,17 +1,12 @@
 ﻿using Ardalis.Specification;
 using System.Collections.Generic;
-using Vivarni.Domain.Core;
 
 namespace Vivarni.Domain.Core
 {
     /// <summary>
-    /// An entity which is capable of carrying domain events. The infrastructure layer should provide a
-    /// mechanism to trigger domain events after the changes have been saved to the database, preferably
-    /// within a database transaction because the domain event handlers may also apply changes to the
-    /// database. Such a mechanism is provided in <c>Vivarni.Domain.Infrastructure</c>.
+    /// An entity which is capable of carrying domain events.
     /// </summary>
-    /// <seealso href="https://github.com/vivarni/vivarni.domain"/>
-    public interface IEntityWithDomainEvents<TId> : IEntity<TId>
+    public interface IEntityWithDomainEvents
     {
         /// <summary>
         /// List of events carried by this <see cref="IEntity{TId}"/>.
@@ -19,4 +14,13 @@ namespace Vivarni.Domain.Core
         /// </summary>
         public List<IDomainEvent> Events { get; }
     }
+
+    /// <summary>
+    /// An entity which is capable of carrying domain events. The infrastructure layer should provide a
+    /// mechanism to trigger domain events after the changes have been saved to the database, preferably
+    /// within a database transaction because the domain event handlers may also apply changes to the
+    /// database. Such a mechanism is provided in <c>Vivarni.Domain.Infrastructure</c>.
+    /// </summary>
+    /// <seealso href="https://github.com/vivarni/vivarni.domain"/>
+    public interface IEntityWithDomainEvents<TId> : IEntity<TId>, IEntityWithDomainEvents { }
 }
