aa4660f7f541467cd5624aed47157d22911bb0b6
Michaël Vittorelli <vittorelli@gmail.com>
2022-03-24 23:27:29 +0100

    VivarniInfrastructureOptionsBuilder
    Co-authored-by: Clivar <16317699+Clivar@users.noreply.github.com>

---
+++ src/Vivarni.DDD.Infrastructure/VivarniInfrastructureOptionsBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Vivarni.DDD.Infrastructure.Caching;
+
+namespace Vivarni.DDD.Infrastructure
+{
+    public class VivarniInfrastructureOptionsBuilder
+    {
+        public Type CachingProviderType { internal get; set; } = typeof(CachingProviderStub);
+        internal ServiceLifetime CachingProviderServiceLifetime { get; set; }
+
+        public void AddCachingProvider<T>(ServiceLifetime lifeTime = ServiceLifetime.Scoped)
+            where T : ICachingProvider
+        {
+            CachingProviderType = typeof(T);
+            CachingProviderServiceLifetime = lifeTime;
+        }
+    }
+}
