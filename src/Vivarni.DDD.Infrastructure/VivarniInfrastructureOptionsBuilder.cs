0604be5eebc1ea3ac3508390f0221ea5b63216d0
Michaël Vittorelli <vittorelli@gmail.com>
2022-03-25 00:15:16 +0100

    Add documentation

--- src/Vivarni.DDD.Infrastructure/VivarniInfrastructureOptionsBuilder.cs
+++ src/Vivarni.DDD.Infrastructure/VivarniInfrastructureOptionsBuilder.cs
@@ -4,11 +4,24 @@ using Vivarni.DDD.Infrastructure.Caching;
 
 namespace Vivarni.DDD.Infrastructure
 {
+    /// <summary>
+    /// Configures the Vivarni infrastructure options.
+    /// </summary>
     public class VivarniInfrastructureOptionsBuilder
     {
+        /// <summary>
+        /// The <see cref="ICachingProvider"/> type used by <see cref="GenericRepository{T}"/>.
+        /// </summary>
         public Type CachingProviderType { internal get; set; } = typeof(CachingProviderStub);
+
+        /// <summary>
+        /// Service lifetime for the <see cref="ICachingProvider"/> used by <see cref="GenericRepository{T}"/>.
+        /// </summary>
         internal ServiceLifetime CachingProviderServiceLifetime { get; set; }
 
+        /// <summary>
+        /// Configures the <see cref="ICachingProvider"/> type to be used by <see cref="GenericRepository{T}"/> with the provided service <paramref name="lifeTime"/>.
+        /// </summary>
         public void AddCachingProvider<T>(ServiceLifetime lifeTime = ServiceLifetime.Scoped)
             where T : ICachingProvider
         {
