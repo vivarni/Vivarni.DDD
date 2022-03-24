39b7fca37ebbe4b9005d40f59948e9874aa665b5
Michaël Vittorelli <vittorelli@gmail.com>
2022-03-24 22:25:51 +0100

    AddFVivarniInfrastructure

---
+++ src/Vivarni.DDD.Infrastructure/ServiceCollectionExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Vivarni.DDD.Core.Repositories;
+using Vivarni.DDD.Infrastructure.DomainEvents;
+
+namespace Vivarni.DDD.Infrastructure
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddFVivarniInfrastructure(this IServiceCollection @this, IConfiguration configuration)
+        {
+            @this.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            @this.AddScoped(typeof(IDomainEventBrokerService), typeof(DomainEventBrokerService));
+
+            return @this;
+        }
+    }
+}
