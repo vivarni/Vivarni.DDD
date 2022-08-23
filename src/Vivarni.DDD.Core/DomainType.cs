29c546b9e2dd425e03032dabe6f6caead7613c33
Michaël Vittorelli <vittorelli@gmail.com>
2022-08-23 16:30:20 +0200

    DomainType: Override equality operators

--- src/Vivarni.DDD.Core/DomainType.cs
+++ src/Vivarni.DDD.Core/DomainType.cs
@@ -26,5 +26,32 @@
         {
             return Name;
         }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+        /// <summary />
+        public static bool operator !=(DomainType lhs, DomainType rhs) => !(lhs == rhs);
+
+        /// <summary />
+        public static bool operator ==(DomainType lhs, DomainType rhs)
+        {
+            if (lhs is null)
+            {
+                if (rhs is null)
+                {
+                    // null == null = true.
+                    return true;
+                }
+
+                // Only the left side is null.
+                return false;
+            }
+            // Equals handles the case of null on right side.
+            return lhs.Name.Equals(rhs.Name);
+        }
     }
 }
