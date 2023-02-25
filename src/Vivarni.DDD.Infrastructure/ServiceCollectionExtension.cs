c29be3707b8698f37b72fb820ddb6f70b6be3d5e
Michaël Vittorelli <vittorelli@gmail.com>
2023-02-25 14:50:27 +0100

    Infrastructure: Register IMemoryCache for default implementation of ICachingProvider if nessecary

--- src/Vivarni.DDD.Infrastructure/ServiceCollectionExtension.cs
+++ src/Vivarni.DDD.Infrastructure/ServiceCollectionExtension.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Vivarni.DDD.Core.Repositories;
 using Vivarni.DDD.Infrastructure.Caching;
 using Vivarni.DDD.Infrastructure.DomainEvents;
@@ -16,12 +18,11 @@ namespace Vivarni.DDD.Infrastructure
         /// </summary>
         public static IServiceCollection AddVivarniInfrastructure(this IServiceCollection @this, Action<VivarniInfrastructureOptionsBuilder>? optionsBuilder = null)
         {
-            @this.AddScoped(typeof(IDomainEventBrokerService), typeof(DomainEventBrokerService));
-
             var options = new VivarniInfrastructureOptionsBuilder();
-            if (optionsBuilder != null)
-                optionsBuilder.Invoke(options);
-            @this.Add(new ServiceDescriptor(typeof(ICachingProvider), options.CachingProviderType, options.CachingProviderServiceLifetime));
+            optionsBuilder?.Invoke(options);
+
+            @this.AddScoped(typeof(IDomainEventBrokerService), typeof(DomainEventBrokerService));
+            @this.AddCachingProvider(options);
 
             options.GenericRepositories.TryAdd(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             foreach (var genericRepository in options.GenericRepositories)
@@ -30,6 +31,19 @@ namespace Vivarni.DDD.Infrastructure
             }
 
             return @this;
+        }
+
+        /// <summary>
+        /// Registers the configured Vivarni ICachingProvider. The default caching provider
+        /// is <see cref="MemoryCacheCachingProvider"/>. When using the default provider, this
+        /// method will also register <see cref="IMemoryCache"/> if nessecary.
+        /// </summary>
+        private static void AddCachingProvider(this IServiceCollection @this, VivarniInfrastructureOptionsBuilder options)
+        {
+            @this.Add(new ServiceDescriptor(typeof(ICachingProvider), options.CachingProviderType, options.CachingProviderServiceLifetime));
+
+            if (options.CachingProviderType == typeof(MemoryCacheCachingProvider))
+                @this.AddMemoryCache();
         }
     }
 }
