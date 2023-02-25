0c6bf36c77f5ddcf0012ba450183bb36f3dce551
Michaël Vittorelli <vittorelli@gmail.com>
2023-02-25 14:52:57 +0100

    Example app: simplify + get it to work :-)

--- Vivarni.Example.Infrastructure/InfrastructureServicesRegistration.cs
+++ Vivarni.Example.Infrastructure/InfrastructureServicesRegistration.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.Reflection;
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Vivarni.DDD.Core;
+using Vivarni.DDD.Core.Repositories;
 using Vivarni.DDD.Infrastructure;
 using Vivarni.DDD.Infrastructure.DomainEvents;
-using Vivarni.DDD.Core.Repositories;
-using Vivarni.DDD.Core;
 
 namespace Vivarni.Example.Infrastructure;
 
@@ -13,28 +13,27 @@ public static class InfrastructureServicesRegistration
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        var executingAssembly = Assembly.GetExecutingAssembly();
+
+        services.AddDbContext<DbContext, ApplicationDbContext>(options => options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+        services.AddVivarniInfrastructure();
         services.AddScoped<IDomainEventBrokerService, DomainEventBrokerService>();
 
-        services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
-
-        var executingAssembly = Assembly.GetExecutingAssembly(); services.AddDbContext<DbContext, ApplicationDbContext>();
-        services.AddVivarniInfrastructure();
         services.Scan(scan => scan
-        .FromAssemblies(executingAssembly)
-        .AddClasses(classes => classes.AssignableTo(typeof(IGenericRepository<>)))
-        .AsImplementedInterfaces()
-        .WithScopedLifetime());
+            .FromAssemblies(executingAssembly)
+            .AddClasses(classes => classes.AssignableTo(typeof(IGenericRepository<>)))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
         services.Scan(scan => scan
-        .FromAssemblies(Assembly.GetExecutingAssembly())
-        .AddClasses(classes => classes.AssignableTo(typeof(IDomainEventHandler<>)))
-        .AsImplementedInterfaces()
-        .WithScopedLifetime());
+            .FromAssemblies(Assembly.GetExecutingAssembly())
+            .AddClasses(classes => classes.AssignableTo(typeof(IDomainEventHandler<>)))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
         services.Scan(scan => scan
-        .FromAssemblies(executingAssembly)
-        .AddClasses(b => b.Where(type => type.Name.EndsWith("Repository")))
-        .AsImplementedInterfaces()
-        .WithScopedLifetime());
+            .FromAssemblies(executingAssembly)
+            .AddClasses(b => b.Where(type => type.Name.EndsWith("Repository")))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
 
         return services;
     }
