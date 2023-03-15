e1c9029ed1f1aba1e3f9d776d8c316c5a2ccdabb
Michaël Vittorelli <vittorelli@gmail.com>
2023-03-15 11:46:09 +0100

    SpecificationBuilderExtensions: Extension method for forced cache refresh (#18)

--- src/Vivarni.DDD.Core/SpecificationExtensions/SpecificationBuilderExtensions.cs
+++ src/Vivarni.DDD.Core/SpecificationExtensions/SpecificationBuilderExtensions.cs
@@ -11,10 +11,24 @@ namespace Vivarni.DDD.Core.SpecificationExtensions
         /// <summary>
         /// Configures the cached specification to expire after a certain timespan (TTL = Time To Live).
         /// </summary>
-        public static ISpecificationBuilder<T> WithCachingTTL<T>(this ICacheSpecificationBuilder<T> @this, TimeSpan ttl)
+        public static ICacheSpecificationBuilder<T> WithCachingTTL<T>(this ICacheSpecificationBuilder<T> @this, TimeSpan ttl)
             where T : class
         {
             @this.Specification.SetCacheTTL(ttl);
+            return @this;
+        }
+
+        /// <summary>
+        /// Configures the cached specification to forcefully refresh. In other words: the
+        /// specification result associated with the cache key of this specification will
+        /// be forcefully refreshed.
+        /// </summary>
+        public static ICacheSpecificationBuilder<T> WithForcedCacheRefresh<T>(this ICacheSpecificationBuilder<T> @this, bool forceCacheRefresh = true)
+            where T : class
+        {
+            if (forceCacheRefresh)
+                @this.Specification.SetForcedCacheRefreshFlag();
+
             return @this;
         }
     }
