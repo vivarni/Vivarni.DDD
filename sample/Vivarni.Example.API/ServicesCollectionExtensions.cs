89585dfbe6f701b7eb63045d360da201a0f85055
Clivar <16317699+Clivar@users.noreply.github.com>
2023-05-09 22:36:03 +0200

    ExampleApp: Move DomainHandlers to Application layer

--- sample/Vivarni.Example.API/ServicesCollectionExtensions.cs
+++ sample/Vivarni.Example.API/ServicesCollectionExtensions.cs
@@ -1,21 +1,18 @@
-﻿using System.Reflection;
-using Vivarni.DDD.Core;
-using Microsoft.Extensions.DependencyInjection;
-namespace Vivarni.Example.Domain
+﻿namespace Vivarni.Example.API
 {
     public static class ServicesCollectionExtensions
     {
         public static IServiceCollection AddDomainServices(this IServiceCollection services)
         {
-             var executingAssembly = Assembly.GetExecutingAssembly();
-    
+            var executingAssembly = Assembly.GetExecutingAssembly();
+
             services.Scan(scan => scan
             .FromAssemblies(Assembly.GetExecutingAssembly())
             .AddClasses(classes => classes.AssignableTo(typeof(IDomainEventHandler<>)))
             .AsImplementedInterfaces()
             .WithScopedLifetime());
-        
-        return services;
+
+            return services;
         }
     }
 }
--- sample/Vivarni.Example.Domain/Entities/GuestMessage.cs
+++ sample/Vivarni.Example.Domain/Entities/GuestMessage.cs
@@ -36,29 +36,3 @@ public class GuestMessageCreatedEvent : IDomainEvent
 
     public GuestMessage GuestMessage { get; }
 }
-public class GuestMessageCreatedEventHandler : IDomainEventHandler<GuestMessageCreatedEvent>
-{
-    private readonly IGenericRepository<GuestMessagesCounter> _guestMessagesCounterrepository;
-
-    public GuestMessageCreatedEventHandler(IGenericRepository<GuestMessagesCounter> guestMessagesCounterrepository)
-    {
-        _guestMessagesCounterrepository = guestMessagesCounterrepository;
-    }
-    public async Task HandleAsync(GuestMessageCreatedEvent domainEvent, CancellationToken cancellationToken)
-    {
-        GuestMessagesCounter wm = new GuestMessagesCounter();
-        var counts = await _guestMessagesCounterrepository.ListAsync(cancellationToken);
-        if(counts.Count == 0)
-        {
-            wm = new GuestMessagesCounter(){ Count = 1, LastEntryBy = domainEvent.GuestMessage.CreatedBy};
-            await _guestMessagesCounterrepository.AddAsync(wm);
-            return;
-        }
-        int count = counts.Max(x => x.Count) + 1;
-        
-        wm.Count = count;
-        wm.LastEntryBy = domainEvent.GuestMessage.CreatedBy;
-        
-        await _guestMessagesCounterrepository.AddAsync(wm);
-    }
-}
